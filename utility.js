0f8977c36f8f44ab46d6d958b08b5292305ebd14
Pistacchio35 <simonepiserchia2004@gmail.com>
2024-10-04 01:42:51 +0200

    Creazione lista della spesa utilizzando JavaScript

--- utility.js
+++ utility.js
@@ -1,25 +1,27 @@
+//Funzione che va ad aggiungere dei prodotti ad una lista della spesa
+//fino a quando l'utente non decide di interrompere il programma
 function aggiungiProdotto(lista) {
 
     let prodotto = "";
-    let n = 0;
     visualizza = ""
 
     while (true) {
 
+        //richiede in input l'inserimento da parte dell'utente di un prodotto
         prodotto = prompt('Cosa vuoi aggiungere alla lista della spesa?')
 
+        //inserimento nella lista del prodotto
         lista.push(prodotto)
-
-        n = n+1
 
         alert('Prodotto aggiunto correttamente!')
 
         prodotto = prompt('Si desidera proseguire?')
-    
+        
         if (prodotto == 'ESCI' || prodotto == 'esci' || prodotto == 'NO' || prodotto == 'no') break;
     }
 
-    for(let i=0;i<n;i++)
+    //visualizzazione di tutti gli elementi della lista
+    for(let i=0;i<lista.length;i++)
     {
     visualizza = visualizza + ' - ' + lista[i]
     console.log(lista[i]);
